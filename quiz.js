3b6278534fb9cdf1577442a5eb21d6e55969e352
andre-iamarino <andre.iamarino.neto@gmail.com>
2023-12-04 15:58:06 -0300

    .

--- quiz.js
+++ quiz.js
@@ -1,3 +1,109 @@
+const cards = {
+  copas: 
+    ['assets/baralho/1/back.png',
+    'assets/baralho/1/Ah.png',
+    'assets/baralho/1/2h.png',
+    'assets/baralho/1/3h.png',
+    'assets/baralho/1/4h.png',
+    'assets/baralho/1/5h.png',
+    'assets/baralho/1/6h.png',
+    'assets/baralho/1/7h.png',
+    'assets/baralho/1/8h.png',
+    'assets/baralho/1/9h.png',
+    'assets/baralho/1/Th.png',
+    'assets/baralho/1/Jh.png',
+    'assets/baralho/1/Qh.png',
+    'assets/baralho/1/Kh.png'],
+  paus: 
+    ['assets/baralho/1/back.png',
+    'assets/baralho/1/Ac.png',
+    'assets/baralho/1/2c.png',
+    'assets/baralho/1/3c.png',
+    'assets/baralho/1/4c.png',
+    'assets/baralho/1/5c.png',
+    'assets/baralho/1/6c.png',
+    'assets/baralho/1/7c.png',
+    'assets/baralho/1/8c.png',
+    'assets/baralho/1/9c.png',
+    'assets/baralho/1/Tc.png',
+    'assets/baralho/1/Jc.png',
+    'assets/baralho/1/Qc.png',
+    'assets/baralho/1/Kc.png'],
+  espadas:
+    ['assets/baralho/1/back.png',
+    'assets/baralho/1/As.png',
+    'assets/baralho/1/2s.png',
+    'assets/baralho/1/3s.png',
+    'assets/baralho/1/4s.png',
+    'assets/baralho/1/5s.png',
+    'assets/baralho/1/6s.png',
+    'assets/baralho/1/7s.png',
+    'assets/baralho/1/8s.png',
+    'assets/baralho/1/9s.png',
+    'assets/baralho/1/Ts.png',
+    'assets/baralho/1/Js.png',
+    'assets/baralho/1/Qs.png',
+    'assets/baralho/1/Ks.png'],
+  ouros:
+    ['assets/baralho/1/back.png',
+    'assets/baralho/1/Ad.png',
+    'assets/baralho/1/2d.png',
+    'assets/baralho/1/3d.png',
+    'assets/baralho/1/4d.png',
+    'assets/baralho/1/5d.png',
+    'assets/baralho/1/6d.png',
+    'assets/baralho/1/7d.png',
+    'assets/baralho/1/8d.png',
+    'assets/baralho/1/9d.png',
+    'assets/baralho/1/Td.png',
+    'assets/baralho/1/Jd.png',
+    'assets/baralho/1/Qd.png',
+    'assets/baralho/1/Kd.png']
+}
+const perguntas = [
+  {
+    pergunta: 
+      ["Situação 1: Hero open CO, BB paga.",
+      "Situação 2: Hero open CO, SB paga, BB paga.",
+      "Situação 3: Hero open CO, BTN paga, BB paga."],
+    flop: 
+      [cards.copas[13],cards.ouros[8],cards.copas[7]],
+    mao: 
+      [cards.copas[1],cards.paus[13],cards.espadas[3],cards.copas[3],cards.paus[2]],
+    opcoes: ['BET', 'CHECK','FUJA'],
+    respostaCorreta: 0,
+    explicacao: 
+      ['Betamos por ter o K na mão. Se temos a Q no lugar do K também podemos betar vs BB.',
+      'Betamos por ter o K na mão. Se temos a Q no lugar do K não iremos betar.',
+      'Betamos por ter o K na mão. Se temos a Q no lugar betamos somento no HU vs BB.']
+  },
+  {
+    pergunta: 
+      ["TESTE 1/2",
+      "TESTE 2/2"],
+    flop: 
+      [cards.copas[13],cards.ouros[8],cards.copas[7]],
+    mao: 
+      [cards.ouros[1],cards.paus[12],cards.copas[10],cards.ouros[6],cards.espadas[3]],
+    opcoes: ['BET', 'CHECK','FUJA'],
+    respostaCorreta: 1,
+    explicacao: 
+      ['Quando temos BD NFD pegamos mais a free card com mãos medianas. Faltam blockers para a CBET.',
+      'BLA BLA BLA']
+  },
+  {
+    pergunta: 
+      ["Situação 1: TESTE SO TEM 1"],
+    flop: 
+      [cards.copas[13],cards.ouros[8],cards.copas[7]],
+    mao: 
+      [cards.paus[1],cards.espadas[10],cards.ouros[10],cards.espadas[7],cards.espadas[5]],
+    opcoes: ['BET', 'CHECK'],
+    respostaCorreta: 1,
+    explicacao: 
+      ['Podemos cbetar HU por ter o blocker do straight (TT) e o 7 que bloqueia 2 pares. No turn, mandamos mais 1 na dobra do 7, no 6, no 9 e J.']
+  }
+];
 
 function embaralharPerguntas() {
   for (let i = perguntas.length - 1; i > 0; i--) {
@@ -15,12 +121,10 @@ function exibirPergunta() {
 
   const questao = perguntas[perguntaAtual];
   const perguntasHTML = document.getElementById('question').getElementsByTagName('p');
-
   perguntasHTML[0].textContent = questao.pergunta[situacao];
  
   const flopImgHTML = document.getElementById('flop').getElementsByTagName('img');
   for (let i = 0; i < flopImgHTML.length; i++) {
-    console.log("bora " + questao.flop[i]);
     flopImgHTML[i].src = questao.flop[i];
   }
   const maoImgHTML = document.getElementById('mao').getElementsByTagName('img');
