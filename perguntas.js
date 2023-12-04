cc1f2160f54ee36c793f90086aeb43c9e2f9cb2e
andre-iamarino <andre.iamarino.neto@gmail.com>
2023-12-04 15:10:12 -0300

    colocando array cards e perguntas em arquivos externos

---
+++ perguntas.js
@@ -0,0 +1,48 @@
+import cards from './cartas.js';
+
+const perguntas = [
+    {
+      pergunta: 
+        ["Situação 1: Hero open CO, BB paga.",
+        "Situação 2: Hero open CO, SB paga, BB paga.",
+        "Situação 3: Hero open CO, BTN paga, BB paga."],
+      flop: 
+        [cards.copas[13],cards.ouros[8],cards.copas[7]],
+      mao: 
+        [cards.copas[1],cards.paus[13],cards.espadas[3],cards.copas[3],cards.paus[2]],
+      opcoes: ['BET', 'CHECK','FUJA'],
+      respostaCorreta: 0,
+      explicacao: 
+        ['Betamos por ter o K na mão. Se temos a Q no lugar do K também podemos betar vs BB.',
+        'Betamos por ter o K na mão. Se temos a Q no lugar do K não iremos betar.',
+        'Betamos por ter o K na mão. Se temos a Q no lugar betamos somento no HU vs BB.']
+    },
+    {
+      pergunta: 
+        ["TESTE 1/2",
+        "TESTE 2/2"],
+      flop: 
+        [cards.copas[13],cards.ouros[8],cards.copas[7]],
+      mao: 
+        [cards.ouros[1],cards.paus[12],cards.copas[10],cards.ouros[6],cards.espadas[3]],
+      opcoes: ['BET', 'CHECK','FUJA'],
+      respostaCorreta: 1,
+      explicacao: 
+        ['Quando temos BD NFD pegamos mais a free card com mãos medianas. Faltam blockers para a CBET.',
+        'BLA BLA BLA']
+    },
+    {
+      pergunta: 
+        ["Situação 1: TESTE SO TEM 1"],
+      flop: 
+        [cards.copas[13],cards.ouros[8],cards.copas[7]],
+      mao: 
+        [cards.paus[1],cards.espadas[10],cards.ouros[10],cards.espadas[7],cards.espadas[5]],
+      opcoes: ['BET', 'CHECK'],
+      respostaCorreta: 1,
+      explicacao: 
+        ['Podemos cbetar HU por ter o blocker do straight (TT) e o 7 que bloqueia 2 pares. No turn, mandamos mais 1 na dobra do 7, no 6, no 9 e J.']
+    }
+  ];
+
+export default perguntas;
--- quiz.js
+++ quiz.js
@@ -1,54 +1,26 @@
 
-const perguntas = []; 
-
-fetch('dados/dados.json')
-    .then(response => response.json())
-    .then(data => {
-        console.log("passou1");
-        data.forEach(item => {
-            perguntas.push({
-                pergunta: item.pergunta,
-                flop: item.flop,
-                mao: item.mao,
-                opcoes: item.opcoes,
-                respostaCorreta: item.respostaCorreta,
-                explicacao: item.explicacao
-            });
-        });
-        console.log("carregou " + perguntas); 
-        console.log("passou2");
-        embaralharPerguntas();
-        console.log("passou3");
-    })
-    .catch(error => {
-        console.error('Erro ao carregar o arquivo JSON:', error);
-    });
-
+import cards from 'cartas.js';
+import perguntas from 'perguntas.js';
 
 function embaralharPerguntas() {
-  console.log("passou4");  
   for (let i = perguntas.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [perguntas[i], perguntas[j]] = [perguntas[j], perguntas[i]];
-    }
-    console.log("passou5");
-    exibirPergunta();
-    console.log("passou6");
+    const j = Math.floor(Math.random() * (i + 1));
+    [perguntas[i], perguntas[j]] = [perguntas[j], perguntas[i]];
   }
+}
 
 let perguntaAtual = 0;
 let pontuacao = 0;
+let situacao = 0;
+let totalRespondidas = 0;
 
 function exibirPergunta() {
-  console.log("passou7");
+
   const questao = perguntas[perguntaAtual];
-  document.getElementById('question').textContent = questao.pergunta;
-  const opcoesHTML = document.getElementById('options').getElementsByTagName('button');
-  for (let i = 0; i < opcoesHTML.length; i++) {
-    opcoesHTML[i].textContent = questao.opcoes[i];
-  }
+  const perguntasHTML = document.getElementById('question').getElementsByTagName('p');
 
-
+  perguntasHTML[0].textContent = questao.pergunta[situacao];
+ 
   const flopImgHTML = document.getElementById('flop').getElementsByTagName('img');
   for (let i = 0; i < flopImgHTML.length; i++) {
     flopImgHTML[i].src = questao.flop[i];
@@ -57,29 +29,41 @@ function exibirPergunta() {
   for (let i = 0; i < maoImgHTML.length; i++) {
     maoImgHTML[i].src = questao.mao[i];
   }
+  const opcoesHTML = document.getElementById('options').getElementsByTagName('button');
+  for (let i = 0; i < opcoesHTML.length; i++) {
+    opcoesHTML[i].textContent = questao.opcoes[i];
+        if (questao.opcoes.length > 2){
+      opcoesHTML[2].style.display = 'block'
+    } else {
+      opcoesHTML[2].style.display = 'none'
+    }
+  }
+   
+
 }
 
 function responder(opcao) {
-  console.log("passou8");
     const questao = perguntas[perguntaAtual];
     const feedback = document.getElementById('feedback');
     const feedback1 = document.getElementById('feedback1');
     const feedback2 = document.getElementById('feedback2');
     if (opcao === questao.respostaCorreta) {
       feedback.textContent = `Resposta correta!`;
-      feedback2.textContent = `${questao.explicacao}`;
+      feedback2.textContent = `${questao.explicacao[situacao]}`;
       feedback.style.backgroundColor = "#8bc34ab5"; 
       feedback.style.padding = "10px"; 
       feedback.style.borderRadius = "5px"; 
       feedback.style.color = "white";
+      // feedback2.style.color = 'gray';
       document.getElementById('feedback').style.display = 'block';
       document.getElementById('feedback1').style.display = 'none';
       document.getElementById('feedback2').style.display = 'block';
       pontuacao++;
+      totalRespondidas++;
     } else {
       feedback.textContent = `Resposta errada!`;
       feedback1.textContent = `Alternativa correta: ${questao.opcoes[questao.respostaCorreta]}.`;
-      feedback2.textContent = `${questao.explicacao}`;
+      feedback2.textContent = `${questao.explicacao[situacao]}`;
       feedback.style.background = "#ff000085";
       feedback.style.padding = "10px";
       feedback.style.borderRadius = "5px"; 
@@ -87,31 +71,19 @@ function responder(opcao) {
       document.getElementById('feedback').style.display = 'block';
       document.getElementById('feedback1').style.display = 'block';
       document.getElementById('feedback2').style.display = 'block';
+      totalRespondidas++;
     }
+    document.getElementById('labelInfomativo').style.display = 'none';
     document.getElementById('next').style.display = 'block';
     document.getElementById('options').style.display = 'none';
 }
 
-function mudarBackground() {
-  const imagens = [
-    "url('https://img.freepik.com/fotos-gratis/fichas-de-cassino-empilhadas-e-royal-flush-em-fundo-verde_23-2148952342.jpg?w=1380&t=st=1700658878~exp=1700659478~hmac=7dfa64fc91c74120ceb97c32b74641ee8852e5a1b7194ff1371f864f1f749c64')",
-    "url('https://img.freepik.com/vetores-gratis/fichas-de-simbolo-de-casino-premium-de-ouro_1017-30141.jpg?w=1380&t=st=1700663293~exp=1700663893~hmac=9da35c82680898ca3e9c92543a51c9fa7328e3d1ca0d73d5e65495e098b41a0e')",
-    "url('https://img.freepik.com/fotos-gratis/sorte-e-riqueza-garantem-uma-noite-de-sucesso-no-cassino-gerada-pela-ia_188544-55835.jpg?w=1380&t=st=1700663940~exp=1700664540~hmac=0a9e3f5b8fc8941dad8b457226a2aeec358cc14242bd38ddcec8395772d51ecd')",
-    "url('https://img.freepik.com/fotos-premium/o-crupie-de-menina-baralha-cartas-de-poquer-em-um-cassino_96270-21.jpg?w=1380')",
-    "url('https://img.freepik.com/fotos-gratis/cartas-e-fichas-de-poquer-na-mesa-verde_93675-132861.jpg?w=1380&t=st=1700664014~exp=1700664614~hmac=6cef46b6225cc0de00fc81a8a210bc2580faed8841810e2f669031f9f8d47213')",
-    "url('https://img.freepik.com/fotos-premium/cartoes-e-fichas-texas-poker-hold39em-poker-online-player39s-smartphone-na-mesa-de-poker-sala-de-poker-jogo-de-poker-jogos-de-cartas-de-casino-online-estilo-de-revista-de-design-moderno_771426-1942.jpg?w=1380')",
-    "url('https://www.shutterstock.com/shutterstock/photos/583864891/display_1500/stock-photo-casino-gambling-poker-people-and-entertainment-concept-close-up-of-poker-player-with-chips-at-583864891.jpg')",
-    "url('https://img.freepik.com/vetores-gratis/simbolos-de-baralho-de-cassino-fundo-preto_1017-20656.jpg?w=1380&t=st=1700663267~exp=1700663867~hmac=166c070ee1519976b9184f329e71aff1aa30f0a18ee986f8078c536049efda7b')"
-  ];
 
-  const imagemAleatoria = imagens[Math.floor(Math.random() * imagens.length)];
-
-  document.body.style.backgroundImage = imagemAleatoria;
-}
 
 function proximaQuestao() {
-  perguntaAtual++;
-  if (perguntaAtual < perguntas.length) {
+  const questao = perguntas[perguntaAtual];
+  if(situacao != (questao.pergunta.length - 1)){
+    situacao++;
     exibirPergunta();
     document.getElementById('feedback').textContent = '';
     document.getElementById('feedback1').textContent = '';
@@ -120,13 +92,35 @@ function proximaQuestao() {
     document.getElementById('feedback1').style.display = 'none';
     document.getElementById('feedback2').style.display = 'none';
     document.getElementById('next').style.display = 'none';
-    document.getElementById('options').style.display = 'block';
+    // document.getElementById('options').style.display = 'block';
+    document.getElementById('options').style.display = 'grid';
     document.getElementById('labelInfomativo').style.display = 'block';
+    
   } else {
-    const quiz = document.getElementById('quiz');
-    quiz.innerHTML = `<div style="background: #000000d1;width: auto;font-size: 45px;padding: 25px;margin-top: 18%;margin-left: auto;margin-right: auto;width: 39%;border-radius: 13px;font-weight: 800;color: white;"><h2>Acabou mane!</h2>
-                      <p>Você acertou ${pontuacao} de ${perguntas.length} questões.</p></div>`;
+    perguntaAtual++;
+    situacao = 0;
+    if (perguntaAtual < perguntas.length) {
+      exibirPergunta();
+      document.getElementById('feedback').textContent = '';
+      document.getElementById('feedback1').textContent = '';
+      document.getElementById('feedback2').textContent = '';
+      document.getElementById('feedback').style.display = 'none';
+      document.getElementById('feedback1').style.display = 'none';
+      document.getElementById('feedback2').style.display = 'none';
+      document.getElementById('next').style.display = 'none';
+      // document.getElementById('options').style.display = 'block';
+      document.getElementById('options').style.display = 'grid';
+      document.getElementById('labelInfomativo').style.display = 'block';
+    } else {
+      const quiz = document.getElementById('quiz');
+      quiz.innerHTML = `<div style="background: #000000d1;width: auto;font-size: 45px;padding: 25px;margin-top: 18%;margin-left: auto;margin-right: auto;width: 39%;border-radius: 13px;font-weight: 800;color: white;"><h2>Acabou mane!</h2>
+                        <p>Você acertou ${pontuacao} de ${totalRespondidas} questões.</p></div>`;
+    }
   }
+  
 }
+
+embaralharPerguntas();
+exibirPergunta();
 
 
